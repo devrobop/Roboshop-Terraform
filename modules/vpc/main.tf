72d9869b51aa347918b1aa6de04a6fce2a81b223
prudhvi-1122 <prudhwi1998@gmail.com>
2024-08-03 21:44:39 +0530

    syntax error

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -198,7 +198,7 @@ resource "aws_eip" "ngw-ip" {
 
 resource "aws_nat_gateway" "main" {
   count         = length(var.availability_zones)
-  allocation_id = aws_eip-ngw-ip.*.id[count.index]
+  allocation_id = aws_eip.ngw-ip.*.id[count.index]
   subnet_id     = aws_subnet.public.*.id[count.index]
 
   tags = {

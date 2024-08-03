73c060059809f0a4096c1d718eaa693c2e9b87af
prudhvi-1122 <prudhwi1998@gmail.com>
2024-08-03 21:41:32 +0530

    yes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,10 +10,10 @@ resource "aws_vpc" "main" {
 
 }
 ## PEERING
-resource "aws_vpc_peering_connection" "name" {
+resource "aws_vpc_peering_connection" "main" {
   peer_vpc_id    = aws_vpc.main.id
   vpc_id         = var.default_vpc_id
-  auto_accept    =  true
+  auto_accept    = true
 }
 
 
@@ -24,22 +24,22 @@ resource "aws_vpc_peering_connection" "name" {
 
 ## SUBNET
 resource "aws_subnet" "web" {
-    count = length(var.web_subnets)
-    vpc_id =  aws_vpc.main.id
-    cidr_block = var.web_subnets[count.index]
+    count             = length(var.web_subnets)
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = var.web_subnets[count.index]
     availability_zone = var.availability_zones[count.index]
 
 
     tags = {
-        Name = "web-subnet-${split("-", var.availability_zones[count.index])[2]}"
+      Name = "web-subnet-${split("-", var.availability_zones[count.index])[2]}"
     }
 }
 
 resource "aws_subnet" "app" {
-    count = length(var.app_subnets)
-    vpc_id =  aws_vpc.main.id
-    cidr_block = var.app_subnets[count.index]
-    availability_zone = var.availability_zones[count.index]
+    count                = length(var.app_subnets)
+    vpc_id               =  aws_vpc.main.id
+    cidr_block           = var.app_subnets[count.index]
+    availability_zone    = var.availability_zones[count.index]
 
 
     tags = {
@@ -48,9 +48,9 @@ resource "aws_subnet" "app" {
 }
 
 resource "aws_subnet" "db" {
-    count = length(var.db_subnets)
-    vpc_id =  aws_vpc.main.id
-    cidr_block = var.db_subnets[count.index]
+    count             = length(var.db_subnets)
+    vpc_id            =  aws_vpc.main.id
+    cidr_block        = var.db_subnets[count.index]
     availability_zone = var.availability_zones[count.index]
 
 
@@ -59,10 +59,10 @@ resource "aws_subnet" "db" {
     }
 }
 resource "aws_subnet" "public" {
-    count = length(var.public_subnets)
-    vpc_id =  aws_vpc.main.id
-    cidr_block = var.public_subnets[count.index]
-    availability_zone = var.availability_zones[count.index]
+    count               = length(var.public_subnets)
+    vpc_id              =  aws_vpc.main.id
+    cidr_block          = var.public_subnets[count.index]
+    availability_zone   = var.availability_zones[count.index]
 
 
     tags = {
@@ -73,15 +73,16 @@ resource "aws_subnet" "public" {
 # ROUTE_TABLE
 
 resource "aws_route_table" "public" {
-  count = length(var.public_subnets)
+  count  = length(var.public_subnets)
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
+
   route {
-    cidr_block = var.default_vpc_cidr
+    cidr_block                = var.default_vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
   
@@ -189,7 +190,7 @@ resource "aws_internet_gateway" "main" {
 }  
 ## NAT GATEWAY
 
-resource "aws_eip" "ngw" {
+resource "aws_eip" "ngw-ip" {
   count = length(var.availability_zones)
   domain  = "vpc"
 }
@@ -201,7 +202,7 @@ resource "aws_nat_gateway" "main" {
   subnet_id     = aws_subnet.public.*.id[count.index]
 
   tags = {
-    Name = "nat-gw-$(split("-", var.availablity_zone[count.index])[2]}"
+    Name = "nat-gw-$(split("-", var.availablity_zones[count.index])[2]}"
   }
 
 }

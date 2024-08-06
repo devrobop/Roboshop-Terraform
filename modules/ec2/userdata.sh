0aac2f3a5dd42340894978e805539613731cfa34
prudhvi-1122 <prudhwi1998@gmail.com>
2024-08-06 21:18:17 +0530

    chamges

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
-ansible-pull -i loacalhost, -U https://github.com/raghudevopsb80/roboshop-ansible main.yml -e env=${env} 
--e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
+ansible-pull -i loacalhost, -U https://github.com/raghudevopsb80/roboshop-ansible main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
 

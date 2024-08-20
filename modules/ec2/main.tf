47dbdfb544901a37377351ccf976a2c3f044a67a
prudhvi-1122 <prudhwi1998@gmail.com>
2024-08-20 16:23:42 +0530

    lookup

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -119,7 +119,7 @@ resource "aws_security_group" "load_balancer" {
     from_port        = 80
     to_port          = 80
     protocol         = "TCP"
-    cidr_blocks      = var.allow_sg_cidr
+    cidr_blocks      = var.allow_lb.sg_cidr
  }
 
 
@@ -161,3 +161,13 @@ resource "aws_lb_listener" "front_end" {
      target_group_arn = aws_lb_target_group.main.*.arn[count.index]
    }
 }  
+resource "aws_route53_record" "lb" {
+  count   = var.asg ? 1 : 0
+  zone_id = var.zone_id
+  name    = "${var.name}.${var.env}"
+  type    =  "CNAME"
+  ttl     = 10
+  records = [aws_lb.main.*.dns_name[count.index]]
+
+
+}

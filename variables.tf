2480ebda5bde2bff011253b9f70f22383bf12c5a
prudhvi-1122 <prudhwi1998@gmail.com>
2024-09-05 17:59:32 +0530

    cluster create

--- variables.tf
+++ variables.tf
@@ -1,8 +1,6 @@
 variable "vpc" {}
 variable "env" {}
-variable "apps" {}
 variable "bastion_nodes" {}
 variable "db" {}
 variable "vault_token" {}
 variable "zone_id" {}
-variable "load_balancers" {}
